755dcbf532a050318074051cfe8d577546bb73d9
jtusetganiello <george.zero@gmail.com>
2021-04-14 12:35:23 -0300

    Is there any betty syntax checker in Python? Asking for a friend

--- aux_functions2.c
+++ aux_functions2.c
@@ -8,14 +8,14 @@
 
 void changing_tab(char *string)
 {
-    int len = _strlen(string);
-    int i = 0;
+	int len = _strlen(string);
+	int i = 0;
 
-    for (; i < len; i++)
-    {
-        if (string[i] == '\t')
-            string[i] = ' ';
-    }
+	for (; i < len; i++)
+	{
+		if (string[i] == '\t')
+			string[i] = ' ';
+	}
 }
 
 /**
@@ -24,35 +24,35 @@ void changing_tab(char *string)
  * Return: count
  */
 
-int count_spaces( char *string)
+int count_spaces(char *string)
 {
-    int len = _strlen(string);
-    int i = 0;
-    int count = 0;
+	int len = _strlen(string);
+	int i = 0;
+	int count = 0;
 
-    for (; i < len; i++)
-    {
-        if (string[i] == ' ' && string[i + 1] != ' ' && string[i + 1] != '\n')
-            count += 1;
-    }
-    return(count + 1);
+	for (; i < len; i++)
+	{
+	if (string[i] == ' ' && string[i + 1] != ' ' && string[i + 1] != '\n')
+		count += 1;
+	}
+	return (count + 1);
 }
 /**
- * count_spaces - count spaces
+ * count_paths - count spaces
  * @string: input string passed
- * Return: count
+ * Return: count + 1
  */
 
-int count_paths( char *string)
+int count_paths(char *string)
 {
-    int len = _strlen(string);
-    int i = 0;
-    int count = 0;
+	int len = _strlen(string);
+	int i = 0;
+	int count = 0;
 
-    for (; i < len; i++)
-    {
-        if (string[i] == ':')
-            count += 1;
-    }
-    return(count + 1);
+	for (; i < len; i++)
+	{
+		if (string[i] == ':')
+			count += 1;
+	}
+	return (count + 1);
 }
